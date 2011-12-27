c5c9142142542c542e499a5ab5c9e36fc01e841d
Jason Britton <jasonbritton@bikegallery.com>
2011-12-27 14:34:00 -0800

    basic layout for the opening page set up. page-opening has all the header and footer in it to make it easier to move back when this page is obsolete.

--- wordpress/wp-content/themes/epictrishop/footer.php
+++ wordpress/wp-content/themes/epictrishop/footer.php
@@ -13,9 +13,12 @@
 
 			</section><!-- .tencol .content -->
 		</section><!-- .row .last -->
+		
+		<footer class="row">
+			Part of the <a href="http://bikegallery.com" title="Bike Gallery">Bike Gallery</a> family.
+			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+		</footer><!-- .row -->
 	</section><!-- .container -->
-	<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-	<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
 
 <?php wp_footer(); ?>
 
--- wordpress/wp-content/themes/epictrishop/page-opening.php
+++ wordpress/wp-content/themes/epictrishop/page-opening.php
@@ -1,32 +1,77 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The Header for our theme.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the wordpress construct of pages
- * and that other 'pages' on your wordpress site will use a
- * different template.
  *
  * @package WordPress
- * @subpackage Starkers
- * @since Starkers 3.0
+ * @subpackage epictrishop
+ * @since epictrishop 0.1
  */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 
-get_header(); ?>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+		wp_head();
+	?>
+</head>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<body <?php body_class(); ?>>
 
-		<?php if ( is_front_page() ) { ?>
-			<h2><?php the_title(); ?></h2>
-		<?php } else { ?>	
-			<h1><?php the_title(); ?></h1>
-		<?php } ?>				
+	<section class="container">
+		<header class="header row">
+			<section class="threecol">
+				<img src="/wordpress/wp-content/themes/epictrishop/images/epictrishop_logo.png" alt="Epic Tri Shop" />
+			</section><!-- .threecol -->
+			<section class="ninecol opening last">
+				<h1>
+					Grand Opening on January 8th!
+				</h1>
+				<p><?php bloginfo( 'description' ); ?></p>
 
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
-			<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+			</section><!-- .twelvecol -->
+		</header><!-- .row -->
+
+		<section class="row">
+
+			<section class="threecol content">
+
+				<h3>Store Hours</h3>
+				<ul>
+					<li>Mon&ndash;Sat: 10am to 6pm</li>
+					<li>Sun: Noon to 5pm</li>
+				</ul>
+
+			</section><!-- .threecol .content -->
+
+			<section class="ninecol content last">
+
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 
-<?php endwhile; ?>
+					<?php the_content(); ?>
+					<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+					<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
 
-<?php get_footer(); ?>
+
+				<?php endwhile; ?>
+
+
+			</section><!-- .ninecol .content -->
+		</section><!-- .row .last -->
+		
+		<footer class="row">
+			Part of the <a href="http://bikegallery.com" title="Bike Gallery">Bike Gallery</a> family
+		</footer><!-- .row -->
+	</section><!-- .container -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
